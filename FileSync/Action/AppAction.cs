ac36af89a0436666464b60fb525e4211201343bb
Patryk Łazowski <jakosziewski@gmail.com>
2023-02-13 20:08:45 +0100

    Add project files.

---
+++ FileSync/Action/AppAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSync.Action
+{
+	interface AppAction
+	{
+		enum Result
+		{
+			Success = 0,
+			Failure = 1
+		}
+
+		Result Perform(string url);
+	}
+}
