965c2b8560b16e721f16e1749361ccd22b96d026
Patryk Łazowski <jakosziewski@gmail.com>
2023-02-13 22:31:25 +0100

    Removed unused imports

--- FileSync/Action/Upload.cs
+++ FileSync/Action/Upload.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CloudService;
-using Utility.CloudService;
+﻿using CloudService;
 
 namespace Utility.Action
 {
--- FileSync/AppActionMapper.cs
+++ FileSync/AppActionMapper.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CloudService;
 using Utility.Action;
 using Utility.CloudService;
